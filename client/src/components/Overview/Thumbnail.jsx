5b1e7421786073b0dfa6fc87de80990d69070a62
Jeffrey Zhang <jzhang22la@gmail.com>
2022-12-01 15:13:28 -0800

    angry red lines

--- client/src/components/Overview/Thumbnail.jsx
+++ client/src/components/Overview/Thumbnail.jsx
@@ -1,12 +1,14 @@
+/* eslint-disable no-var */
+/* eslint-disable react/function-component-definition */
 import React from 'react';
 
 const Thumbnail = () => {
-  //for selected image...
-    //highlight/outline/add some kind of checkmark
-  var thumbnail_url = 'placeholder'
+  // for selected image...
+  // highlight/outline/add some kind of checkmark
+  var thumbnail_url = 'placeholder';
   return (
     <div>
-      <img src={thumbnail_url} />
+      <img src={thumbnail_url} alt="" />
 
     </div>
   );
