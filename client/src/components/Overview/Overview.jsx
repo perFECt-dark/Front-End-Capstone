1979b3a5c7a49f4b4e94ad3568a36e1adbf05aa5
Jeffrey Zhang <jzhang22la@gmail.com>
2022-11-30 21:02:27 -0800

    deleted index because it adds another script everytime you build your webpack, and initialized an overview folder

---
+++ client/src/components/Overview/Overview.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Overview = () => {
+  /*
+  going to whiteboard some things first
+  I guess the general idea of my component is the largest display of:
+     images, product info, styles and the ability to add to cart
+  */
+  return (
+
+  );
+};
+
+export default Overview;
