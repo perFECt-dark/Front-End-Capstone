f90ac93095c18b530efbc1ddd2819c02455ceb78
JustDatGuy <BAnusiem@gmail.com>
2022-12-02 12:30:49 -0800

    created card component, issues rendering

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,78 @@
 import React from 'react';
 
+import Card from './Card.jsx';
+
 function App() {
+  // const sampleData = {
+  //   "product_id": "1",
+  //   "results": [
+  //     {
+  //       "style_id": 1,
+  //       "name": "Forest Green & Black",
+  //       "original_price": "140",
+  //       "sale_price": "0",
+  //       "default?": true,
+  //       "photos": [
+  //         {
+  //           "thumbnail_url": "urlplaceholder/style_1_photo_number_thumbnail.jpg",
+  //           "url": "urlplaceholder/style_1_photo_number.jpg"
+  //         },
+  //         {
+  //           "thumbnail_url": "urlplaceholder/style_1_photo_number_thumbnail.jpg",
+  //           "url": "urlplaceholder/style_1_photo_number.jpg"
+  //         }
+  //         // ...
+  //       ],
+  //       "skus": {
+  //         "37": {
+  //           "quantity": 8,
+  //           "size": "XS"
+  //         },
+  //         "38": {
+  //           "quantity": 16,
+  //           "size": "S"
+  //         },
+  //         "39": {
+  //           "quantity": 17,
+  //           "size": "M"
+  //         },
+  //         //...
+  //       }
+  //     },
+  //     {
+  //       "style_id": 2,
+  //       "name": "Desert Brown & Tan",
+  //       "original_price": "140",
+  //       "sale_price": "0",
+  //       "default?": false,
+  //       "photos": [
+  //         {
+  //           "thumbnail_url": "urlplaceholder/style_2_photo_number_thumbnail.jpg",
+  //           "url": "urlplaceholder/style_2_photo_number.jpg"
+  //         }
+  //         // ...
+  //       ],
+  //       "skus": {
+  //         "37": {
+  //           "quantity": 8,
+  //           "size": "XS"
+  //         },
+  //         "38": {
+  //           "quantity": 16,
+  //           "size": "S"
+  //         },
+  //         "39": {
+  //           "quantity": 17,
+  //           "size": "M"
+  //         },
+  //       }
+  //     },
+  //   ]
+  // };
   return (
     <div>
-      Test
+      {/* <Card product={sampleData} /> */}
+      Hello
     </div>
   );
 }
---
+++ client/src/components/Card.jsx
@@ -0,0 +1,28 @@
+//import propTypes from 'prop-types';
+import React from 'react';
+
+function Card({ product }) {
+  return (
+    // <div className="card">
+    //   <div className="image-card">
+    //     <img className="image-object" alt={`for ${product.results.name}`} src={product.results.photos[0].url} />
+    //   </div>
+    //   <div className="card-title">
+    //     {product.results.name}
+    //     <br />
+    //     Orignial price:
+    //     {product.results.original_price}
+    //     <br />
+    //     Sale price:
+    //     {product.results.sale_price}
+    //   </div>
+    // </div>
+    <div>Hello</div>
+  );
+}
+
+// Card.propTypes = {
+//   product: propTypes.objectOf(propTypes.object()).isRequired,
+// };
+
+export default Card;
